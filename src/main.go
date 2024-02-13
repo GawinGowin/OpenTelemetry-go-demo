f60bc61d398c0ddd27830c900c286db0ddc05e40
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2024-02-13 06:55:21 +0000

    Add OpenTelemetry instrumentation for HTTP requests

--- src/main.go
+++ src/main.go
@@ -1,12 +1,67 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
-func main(){
-	http.HandleFunc("/", rolldice)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func main() {
+	if err := run(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func run() (err error) {
+	// Handle SIGINT (CTRL+C) gracefully.
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	otelShutdown, err := setupOTelSDK(ctx)
+	if err != nil {
+		return
+	}
+	defer func() {
+		err = errors.Join(err, otelShutdown(ctx))
+	} ()
+	
+	srv := &http.Server{
+		Addr: ":8080",
+		BaseContext: func(_ net.Listener) context.Context { return ctx },
+		ReadTimeout: time.Second,
+		WriteTimeout: 10 * time.Second,
+		Handler: newHTTPHandler(),
+	}
+	srvErr := make(chan error, 1)
+	go func() {
+		srvErr <- srv.ListenAndServe()
+	} ()
+
+	select {
+		case err = <-srvErr:
+			return
+		case <-ctx.Done():
+			stop()
+	}
+
+	err = srv.Shutdown(context.Background())
+	return
+}
+
+func newHTTPHandler() http.Handler {
+	mux := http.NewServeMux()
+	handleFunc := func(pattern string, handlerFunc func(http.ResponseWriter, *http.Request)) {
+		handler := otelhttp.WithRouteTag(pattern, http.HandlerFunc(handlerFunc))
+		mux.Handle(pattern, handler)
+	}
+	handleFunc("/rolldice", rolldice)
+	handler := otelhttp.NewHandler(mux, "/")
+	return handler
 }
